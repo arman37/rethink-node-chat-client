0b650ea57ba833fe72f9c0dd6c9df00e84f5639f
arman37 <arman.it37@gmail.com>
2017-11-29 19:54:25 +0600

    Add login handler function

--- app/utility/api.js
+++ app/utility/api.js
@@ -8,3 +8,32 @@
 
 import $ajax from './ajax';
 import utils from './utils';
+
+/**
+ * Handle user login.
+ *
+ * @param username
+ * @param password
+ * @returns {Promise.<TResult>}
+ */
+function handleLogin(username, password) {
+  return (
+    $ajax({
+      url: 'users/actions/login',
+      method: 'POST',
+      body: {
+        username: username,
+        password: password
+      }
+    })
+    .then(response => response)
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    })
+  );
+}
+
+export default {
+  handleLogin
+};
