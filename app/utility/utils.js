1c6a7ea056e3069e0c44ce599578e0dd499d71da
arman37 <arman.it37@gmail.com>
2017-11-29 19:34:27 +0600

    Add getToken, setToken, removeToken functions

--- app/utility/utils.js
+++ app/utility/utils.js
@@ -7,5 +7,9 @@
 'use strict';
 
 export default {
+  setToken: token => localStorage.setItem('token', token),
 
+  getToken: () => localStorage.getItem('token'),
+
+  removeToken: () => localStorage.removeItem('token'),
 }
