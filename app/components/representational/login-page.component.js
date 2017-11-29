63399dcbdf9beb1e65e79824c0a54ebffb0d62c7
arman37 <arman.it37@gmail.com>
2017-11-29 19:53:18 +0600

    Update login page component

--- app/components/representational/login-page.component.js
+++ app/components/representational/login-page.component.js
@@ -9,9 +9,15 @@ import React from 'react';
 import Header from './header.component';
 import LoginForm from './login-form.component';
 
+/**
+ *
+ * @param {function} handleSubmit
+ * @param {function} handleChange
+ * @constructor
+ */
 const LoginPage = ({handleSubmit, handleChange}) => (
   <div>
-    <Header></Header>
+    <Header title='A RethinkDB chat application with React, NodeJS, HapiJS and socket.io' />
     <LoginForm
       handleChange={handleChange}
       handleSubmit={handleSubmit} />
