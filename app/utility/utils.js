d74b3d10e7bf409db6719506a52bf37e8f9a5062
arman37 <arman.it37@gmail.com>
2017-11-29 19:47:02 +0600

    Add jwt parser

--- app/utility/utils.js
+++ app/utility/utils.js
@@ -12,4 +12,10 @@ export default {
   getToken: () => localStorage.getItem('token'),
 
   removeToken: () => localStorage.removeItem('token'),
+
+  parseJwt: (token) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+  }
 }
