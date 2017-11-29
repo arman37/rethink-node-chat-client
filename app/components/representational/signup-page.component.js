ad56f6e2c4b7d2213cb02ababdf4f761001bf72d
arman37 <arman.it37@gmail.com>
2017-11-29 20:09:24 +0600

    Update signup page component

--- app/components/representational/signup-page.component.js
+++ app/components/representational/signup-page.component.js
@@ -9,13 +9,19 @@ import React from 'react';
 import Header from './header.component';
 import SignUpForm from './signup-form.component';
 
+/**
+ *
+ * @param {function} handleSubmit
+ * @param {function} handleChange
+ * @constructor
+ */
 const SignUpPage = ({handleSubmit, handleChange}) => (
-    <div>
-        <Header></Header>
-        <SignUpForm
-            handleChange={handleChange}
-            handleSubmit={handleSubmit} />
-    </div>
+  <div>
+    <Header title='A RethinkDB chat application with React, NodeJS, HapiJS and socket.io' />
+    <SignUpForm
+      handleChange={handleChange}
+      handleSubmit={handleSubmit} />
+  </div>
 );
 
 export default SignUpPage;
