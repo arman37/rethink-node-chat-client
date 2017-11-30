a1208155e151b34ff1735e91ae00cbc7130e84b5
arman37 <arman.it37@gmail.com>
2017-11-30 16:50:22 +0600

    Update user-list component

--- app/components/representational/user-list.component.js
+++ app/components/representational/user-list.component.js
@@ -4,14 +4,29 @@
  *
  */
 'use strict';
-
 import React from 'react';
+import Title from './title.component';
 import {List, ListItem} from 'material-ui/List';
 import Face from 'material-ui/svg-icons/action/face';
 
-const UserList = ({connectedUsers}) => (
-  <div className="online__users">
-    <span className="title">Online Users</span>
+const styles = {
+  root: {
+    width: '20%',
+    display: 'inline-block',
+    float: 'left',
+    borderStyle: 'inset'
+  }
+};
+
+/**
+ *
+ * @param {array} connectedUsers
+ * @param {string} className
+ * @constructor
+ */
+const UserList = ({connectedUsers, className}) => (
+  <div className={className} style={styles.root}>
+    <Title title='Online Users' className='online__users' />
     <List>
       {
         connectedUsers.map((user, index) => (
