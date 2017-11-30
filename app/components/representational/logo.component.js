214fa2575cab1026ef0d6c7c273ee1e45e1c6370
arman37 <arman.it37@gmail.com>
2017-11-30 19:32:41 +0600

    Add inline styles to logo component

--- app/components/representational/logo.component.js
+++ app/components/representational/logo.component.js
@@ -8,8 +8,15 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 
+const styles = {
+  root: {
+    margin: '0 auto 0 auto',
+    width: '30%'
+  }
+};
+
 const Logo = () => (
-  <Paper className="app-logo-wrapper" zDepth={0}>
+  <Paper className="app__logo__wrapper" zDepth={0} style={styles.root}>
     <img src="./images/chat_ico.png" height="90" width="130"/>
   </Paper>
 );
