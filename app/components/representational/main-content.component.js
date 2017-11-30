308a9f725570722eb8eb78eeca5a80391f2087a6
arman37 <arman.it37@gmail.com>
2017-11-30 19:36:39 +0600

    Update main-content component

--- app/components/representational/main-content.component.js
+++ app/components/representational/main-content.component.js
@@ -7,37 +7,47 @@
 
 import React from 'react';
 import RoomList from './room-list.component';
-import UserList from './user-list.component';
+import UserList from './online-users.component';
 import Messages from './messages.component';
-import CreateRoom from './create-room.component';
+import CreateRoom from './create-room-dialog.component';
 import Confirmation from './confirmation.component';
 
-const MainContent = ({state, progress,newMessage, chatRoomList, currentRoom, createRoom, handleChange, handleMessageSend, handleRoomClick, confirmOpen, confirmMessage, handleRoomSubmit, connectedUsers, handleCreateRoomOpen}) => (
-  <div className="main__content">
-    <div className="list__view">
-      <UserList
-        connectedUsers={connectedUsers} />
-      <Messages
-        state={state}
-        newMessage={newMessage}
-        currentRoom={currentRoom}
+const MainContent = (props) => {
+  let {
+    state, progress, newMessage, chatRoomList, currentRoom, createRoom, handleChange, handleMessageSend, handleRoomClick,
+    confirmOpen, confirmMessage, handleRoomSubmit, connectedUsers, toggleCreateRoomDialog
+  } = props;
+
+  return (
+    <div className="main__content">
+      <div className="list__view">
+        <UserList
+          className='online__users'
+          connectedUsers={connectedUsers} />
+        <Messages
+          state={state}
+          className='messages'
+          newMessage={newMessage}
+          currentRoom={currentRoom}
+          handleChange={handleChange}
+          handleMessageSend={handleMessageSend} />
+        <RoomList
+          className='room__list'
+          progress={progress}
+          chatRoomList={chatRoomList}
+          handleRoomClick={handleRoomClick}
+          toggleCreateRoomDialog={toggleCreateRoomDialog} />
+      </div>
+      <CreateRoom
+        createRoom={createRoom}
         handleChange={handleChange}
-        handleMessageSend={handleMessageSend} />
-      <RoomList
-        progress={progress}
-        chatRoomList={chatRoomList}
-        handleRoomClick={handleRoomClick}
-        handleCreateRoomOpen={handleCreateRoomOpen} />
+        handleRoomSubmit={handleRoomSubmit}
+        toggleCreateRoomDialog={toggleCreateRoomDialog} />
+      <Confirmation
+        confirmOpen={confirmOpen}
+        confirmMessage={confirmMessage} />
     </div>
-    <CreateRoom
-      createRoom={createRoom}
-      handleChange={handleChange}
-      handleRoomSubmit={handleRoomSubmit}
-      handleCreateRoomOpen={handleCreateRoomOpen} />
-    <Confirmation
-      confirmOpen={confirmOpen}
-      confirmMessage={confirmMessage} />
-  </div>
-);
+  );
+};
 
 export default MainContent;
