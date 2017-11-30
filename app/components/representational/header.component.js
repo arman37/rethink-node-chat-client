7bc3a8bb1dbad63551fd20d86e4a31b61c6b4e68
arman37 <arman.it37@gmail.com>
2017-11-30 19:16:46 +0600

    Update header style

--- app/components/representational/header.component.js
+++ app/components/representational/header.component.js
@@ -8,13 +8,23 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 
+const styles = {
+  root: {
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '10%',
+    position: 'absolute',
+    textAlign: 'center'
+  }
+};
 /**
  *
  * @param {string} title
  * @constructor
  */
 const Header = ({title}) => (
-  <Paper className="header-wrapper" zDepth={2}>
+  <Paper className="header__wrapper" zDepth={2} style={styles.root}>
     <h1>{title}</h1>
   </Paper>
 );
