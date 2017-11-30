daf981f646e0d66b3e14b251d5c7a23a2ce317fa
arman37 <arman.it37@gmail.com>
2017-11-30 19:01:47 +0600

    Update message list component style

--- app/components/representational/message-list.component.js
+++ app/components/representational/message-list.component.js
@@ -9,8 +9,23 @@ import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Face from 'material-ui/svg-icons/action/face';
 
-const MessageList = ({state, currentRoom}) => (
-  <div className="message__list">
+const styles = {
+  root: {
+    width: '100%',
+    height: '88%',
+    overflowY: 'scroll'
+  }
+};
+
+/**
+ *
+ * @param {object} state
+ * @param {object} currentRoom
+ * @param {string} className
+ * @constructor
+ */
+const MessageList = ({state, currentRoom, className}) => (
+  <div className={className} style={styles.root}>
     <List>
       {
         state && state[currentRoom.id] && state[currentRoom.id].map((message, index) => (
