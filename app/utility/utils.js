adc6901561ae8660654ce0aec2da9763792f0ace
arman37 <arman.it37@gmail.com>
2018-02-26 20:03:11 +0600

    Add moment date formatter

--- app/utility/utils.js
+++ app/utility/utils.js
@@ -6,6 +6,8 @@
  */
 'use strict';
 
+import moment from 'moment';
+
 export default {
   setToken: token => localStorage.setItem('token', token),
 
@@ -17,5 +19,24 @@ export default {
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace('-', '+').replace('_', '/');
     return JSON.parse(window.atob(base64));
+  },
+
+  toMomentCalendarDate: (date) => {
+    let otherDates = moment(date).fromNow();
+    let cb = () => {
+      return '['+otherDates+']';
+    };
+
+    return (
+      moment(date).calendar(null,
+          {
+            sameDay: '[Today] LT',
+            nextDay: '[Tomorrow] LT',
+            nextWeek: 'MMM DD, YYYY LT',
+            lastDay: '[Yesterday] LT',
+            lastWeek: 'MMM DD, YYYY LT',
+            sameElse: 'MMM DD, YYYY LT'
+          })
+    );
   }
 }
